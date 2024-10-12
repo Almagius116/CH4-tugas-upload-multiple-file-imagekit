5c0562e6341138579e02d5b3b5dfd4e44e2bf2ae
unknown <almagiusunggane@gmail.com>
2024-10-12 23:26:46 +0700

    upload car images dengan multer dan imagekit

---
+++ routes/carsRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const upload = require("../middlewares/uploader");
+const carController = require("../controller/carController");
+
+// Cars API
+router.post("/", upload.array("images", 10), carController.createCar);
+router.get("/", carController.getAllCars);
+router.get("/:id", carController.getCarById);
+router.delete("/:id", carController.deleteCarById);
+router.patch("/:id", carController.updateCar);
+
+module.exports = router;
